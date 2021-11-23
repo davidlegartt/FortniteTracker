d5c3d7d5f95acef2f78ad640be1914ec3a227b92
davidlegartt <72855826+davidlegartt@users.noreply.github.com>
2021-11-23 22:28:28 +0200

    Update daily.js

--- daily.js
+++ daily.js
@@ -10,11 +10,13 @@ fetch('https://fortnite-api.com/v2/shop/br')
         console.log(testing);
 
         $.each(data.data.daily.entries, function(i, item) {
-            $('body > #cards_daily').append('<div class="card"> ' + item.items[0].name + ' <br> ' + item.finalPrice + '<img src="https://fortnite-api.com/images/vbuck.png" height="20px">' + ' <br> ' + '<img id="image" src="' + item.items[0].images.icon + '"></img>' + '</div>');
+            console.log(i);
+            $('body > #cards_daily').append('<div class="card"> ' + item.items[0].name + ' <br> <span>' + item.finalPrice + '<img id="v_buck" src="https://fortnite-api.com/images/vbuck.png" height="28px"> </span>' + ' <br> ' + '<img id="image" src="' + item.items[0].images.icon + '"></img>' + '</div>');
+            //console.log(item.items[0].rarity.displayValue + " - " + item.items[0].name)
         });
     });
 
-//Features offers
+//Featured offers
 fetch('https://fortnite-api.com/v2/shop/br')
     .then(res => res.json())
     .then(data => {
@@ -26,6 +28,49 @@ fetch('https://fortnite-api.com/v2/shop/br')
         console.log(testing);
 
         $.each(data.data.featured.entries, function(i, item) {
-            $('body > #cards_featured').append('<div class="card"> ' + item.items[0].name + ' <br> ' + item.finalPrice + ' vBucks' + ' <br> ' + '<img id="image" src="' + item.items[0].images.icon + '"></img>' + '</div>');
+            $('body > #cards_featured').append('<div class="card"> ' + item.items[0].name + ' <br> <span>' + item.finalPrice + '<img id="v_buck" src="https://fortnite-api.com/images/vbuck.png" height="28px"> </span>' + '  <br> ' + '<img id="image" src="' + item.items[0].images.icon + '"></img>' + '</div>');
+
         });
     });
+
+fetch('https://fortnite-api.com/v2/shop/br')
+    .then(res => res.json())
+    .then(data => {
+        //Log fetched json
+        console.log(data)
+
+        const test = JSON.stringify(data.data.specialFeatured.entries)
+        const testing = JSON.parse(test);
+        console.log(testing);
+
+        $.each(data.data.specialFeatured.entries, function(i, item) {
+            $('body > #cards_featured_special').append('<div class="card"> ' + item.items[0].name + ' <br> <span>' + item.finalPrice + '<img id="v_buck" src="https://fortnite-api.com/images/vbuck.png" height="28px"> </span>' + '  <br> ' + '<img id="image" src="' + item.items[0].images.icon + '"></img>' + '</div>');
+
+        });
+    });
+
+//timer
+(function() {
+    var start = new Date;
+    start.setHours(2, 0, 0); // 11pm
+
+    function pad(num) {
+        return ("0" + parseInt(num)).substr(-2);
+    }
+
+    function tick() {
+        var now = new Date;
+        if (now > start) { // too late, go to tomorrow
+            start.setDate(start.getDate() + 1);
+        }
+        var remain = ((start - now) / 1000);
+        var hh = pad((remain / 60 / 60) % 60);
+        var mm = pad((remain / 60) % 60);
+        var ss = pad(remain % 60);
+        document.getElementById('time').innerHTML =
+            hh + ":" + mm + ":" + ss;
+        setTimeout(tick, 1000);
+    }
+
+    document.addEventListener('DOMContentLoaded', tick);
+})();
